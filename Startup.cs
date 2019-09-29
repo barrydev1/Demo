9189e471620a21eec127329b58e9f78ff1a17314
Barry Sharpen <barrysharpen@gmail.com>
2019-09-29 22:05:14 +0100

    Updated message

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace Demo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello Everyone!");
+                await context.Response.WriteAsync("Hello Azure!");
             });
         }
     }
